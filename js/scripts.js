76a9d37a3395a111c6e14aa425f563502fb13b49
Sean Wilson <paintballsean@gmail.com>
2020-08-17 15:18:29 -0700

    Commit adds loop ffor story.
    Co-authored-by: Diego Salazar <salazardiego343@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,24 @@ $(document).ready(function() {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".object").text(objectInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
-    $("#story").show();
-
     event.preventDefault();
+
+   let spanArray = ['person1', 'person2', 'object', 'exclamation', 'verb', 'noun'];
+
+   spanArray.forEach(function(span){
+    console.log(span);
+    let thisInput = $('input#' + span).val();
+      $('.' + span).text(thisInput);
+    });
+    $('#story').show();
   });
 });
+
+/* let newArray = ['tag1', 'tag2', 'tag3', 'tag4'];
+
+newArray.forEach(function(news) {
+  let thisInput = $('input#' + news).val();
+  $('.' + news).text(thisInput);
+})
+
+$('#story').show(); */
